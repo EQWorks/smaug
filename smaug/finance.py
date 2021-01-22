24aac8222dc60ce51922cabaa8266ff91ddb8c30
Runzhou Li (woozyking) <runzhou.li@gmail.com>
2021-01-22 13:12:41 -0500

    finance - revise logs insights query to be path only id compatible, parse API stage

--- smaug/finance.py
+++ smaug/finance.py
@@ -3,10 +3,10 @@
 # TODO: see if logs group can be inferred from serverless/cloudformation config
 query = '''
     fields @message
-    | filter @message like "smaug#id"
-    # | filter @message not like "whitelabel#-1"
-    | parse @message /\[(?<level>\S+)\]\s+(?<ts>\S+)\s+(?<rid>\S+)\s+(?<msg>\S+)/
-    | parse msg /smaug#id#(?<id>\S+)#whitelabel#(?<whitelabel>\S+)#customer#(?<customer>\S+)#key#(?<key>\S+)#n#(?<calls>\S+)/
-    | parse id /(?<api>\S+)\?(?<query>\S+)/
-    | stats sum(calls) as total_calls by whitelabel, customer, api
+        | filter @message like "smaug#id"
+        | parse @message /\[(?<level>\S+)\]\s+(?<ts>\S+)\s+(?<rid>\S+)\s+(?<msg>\S+)/
+        | parse msg /smaug#id#(?<id>\S+)#whitelabel#(?<whitelabel>\S+)#customer#(?<customer>\S+)#key#(?<key>\S+)#n#(?<calls>\S+)/
+        | parse id /(?<endpoint>[^\s\?\#]+)(\?(?<query>\S+))?/
+        | parse endpoint /(?<stage>[^\s\/]+)(?<api>\S+)?/
+        | stats sum(calls) as total_calls by whitelabel, customer, stage, api
 '''
